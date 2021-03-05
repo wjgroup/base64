b57319e1a09f29b71df32a5481289f91321119f8
aw <wj.group@hotmail.com>
2021-03-06 01:09:50 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace base64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            if (args.Length != 2) {
+                return;
+            }
+
+            if (args[0] == "--encode" || args[0] == "-e") {
+                var bytes = System.Text.Encoding.UTF8.GetBytes(args[1]);
+                var text = Convert.ToBase64String(bytes);
+                Console.WriteLine(text);
+
+            }
+            else if (args[0] == "--decode" || args[0] == "-d") {
+                var bytes = Convert.FromBase64String(args[1]);
+                var text = System.Text.Encoding.UTF8.GetString(bytes);
+                Console.WriteLine(text);
+            } 
+            else {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
